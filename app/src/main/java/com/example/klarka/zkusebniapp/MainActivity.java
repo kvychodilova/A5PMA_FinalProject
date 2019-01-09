1963fd3e7fa2de73d8cd9b863379ed51d79e2a6e
Klára Vychodilová <k_vychodilova@utb.cz>
2019-01-09 23:03:27 +0100

    Komunikace s Internetem OK Výpis vázne

--- app/src/main/java/com/example/klarka/zkusebniapp/MainActivity.java
+++ app/src/main/java/com/example/klarka/zkusebniapp/MainActivity.java
@@ -1,20 +1,51 @@
 package com.example.klarka.zkusebniapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Console;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 
 public class MainActivity extends AppCompatActivity {
-    Spinner S1, S2;
+
+    private EditText editText;
+    private Button button;
+    private Spinner S1, S2;
+    private TextView transferResult;
+
+    private String indexS1;
+    private String indexS2;
+    private double inputval;
+
+    private String vysledek;
+
+    private String result[] = new String[10];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +54,17 @@ public class MainActivity extends AppCompatActivity {
 
         S1 = findViewById(R.id.Setter_1);
         S2 = findViewById(R.id.Setter_2);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        transferResult = findViewById(R.id.transferResult);
 
-
-        List<String> money_list = new ArrayList<>();
+        //Add curency to spinners
+       /* List<String> money_list = new ArrayList<>();
         money_list.add("USD");
         money_list.add("EUR");
         money_list.add("GBP");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,money_list);
-
+        */
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.currency, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         S1.setAdapter(adapter);
         S2.setAdapter(adapter);
@@ -40,8 +73,9 @@ public class MainActivity extends AppCompatActivity {
         S1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String fromValue = parent.getItemAtPosition(position).toString();
-                Toast.makeText(MainActivity.this,"Selected:" + fromValue, Toast.LENGTH_SHORT).show();
+                indexS1 = parent.getItemAtPosition(position).toString();
+                //String fromValue = parent.getItemAtPosition(position).toString();
+                //Toast.makeText(MainActivity.this,"Selected:" + indexS1, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -54,7 +88,8 @@ public class MainActivity extends AppCompatActivity {
         S2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String toValue = parent.getItemAtPosition(position).toString();
+                indexS2 = parent.getItemAtPosition(position).toString();
+              //String toValue = parent.getItemAtPosition(position).toString();
             }
 
             @Override
@@ -63,6 +98,87 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                transferResult.setText("Prekladam..");
+                //receive input from  user
+                String vstup = editText.getText().toString();
+                //transfer to double
+                inputval = Double.parseDouble(vstup);
+                /*testovani vstupu OK
+                Toast.makeText(MainActivity.this,"input:" + inputval, Toast.LENGTH_SHORT).show(); */
+
+                new calculate().execute();
+            }
+        });
+
+    }
+
+    public class calculate extends AsyncTask<String, String, String []> {
+
+        @Override
+        protected void onPreExecute() {
+            //pracuje během toho co běží doInBackground
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String [] doInBackground(String... strings) {
+            //nasypat do URL data -> z čeho, na co a kolik
+            String url;
+            try{
+                url=getJson("http://free.currencyconverterapi.com/api/v5/convert?q=EUR_USD&compact=10");
+
+                //url=getJson("https://www.amdoren.com/api/currency.php?api_key=TXD4JQJb9Su2qQa4wzQFwPjHPwsM9E&from="+ indexS1 +"&to="+indexS2+"&amount="+inputval);
+                JSONObject finalValtoObject = new JSONObject(url);
+
+               /*JSONObject amountObject = finalValtoObject.getJSONObject("amount");
+                vysledek = finalValtoObject.getString("amount"); */
+
+               JSONObject amountarray = finalValtoObject.getJSONObject("results").getJSONObject("EUR_USD");
+                result[0] = (String.valueOf(amountarray.getJSONObject(String.valueOf("val"))));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //return vysledek;
+            //System.out.println(vysledek);
+            return result;
+
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            //pracuje po doInBackground
+            //super.onPostExecute(strings);
+            //double vypisvysledek = Double.parseDouble(result[0]);
+            //vypisvysledek = Double.parseDouble(vysledek);
+            //transferResult.setText(vypisvysledek);
+
+            transferResult.setText(result[0]);
+
+        }
+
+        public String getJson(String URL) throws ClientProtocolException, IOException{
+
+                StringBuilder build = new StringBuilder();
+                HttpClient client = new DefaultHttpClient();
+                HttpGet httpGet =  new HttpGet(URL);
+                HttpResponse response = client.execute(httpGet);
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String con;
+
+                while ((con=reader.readLine()) != null){
+                    build.append(con);
+                }
+                return build.toString();
+
+        }
 
 
     }
